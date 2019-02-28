9a04ab4442ee130fa62e13f75441653442a38a3e
RamonMamon <35195972+RamonMamon@users.noreply.github.com>
2019-02-28 22:54:23 +0000

    Implemented the Go Back N protocol into the practical and moved the timeouts from the client's side to be more prominent on the server's side.

--- Client/Client.js
+++ Client/Client.js
@@ -16,34 +16,29 @@ const speaker = new Speaker();
 const port = 41236;
 
 // Protocol messages
-const START_TRANSFER = 'Start Transfer';
-const PACKET_INFO_INDEX = 'Packet Info';
-const PARTITION_PACKET = 'Partition Packet';
-const INITIATE_TRANSFER = 'Initiate Partition Transfer';
-const PARTITION_FINISHED = 'Partition Sent';
-const FILE_TRANSFERRED = 'Finished';
-const MISSING_PACKET = 'Missing Packet'
+const START_TRANSFER = 0;
+const PACKET_INFO_INDEX = 1;
+const PARTITION_PACKET = 2;
+const INITIATE_TRANSFER = 3;
+const PARTITION_FINISHED = 4;
+const FILE_TRANSFERRED = 5;
+const MISSING_PACKET = 6;
 
 const REQUEST_INTERVAL = 1000;
-const RETRY_ATTEMPTS = 10;
 const NO_DATA = 0;
 
+// Server Info
 let serverPort;
 let serverAddress;
 
-let oldPartitionOffset = 0;
 let partitionOffset = 0;
-
 let defaultPartitionSize;
 let requestInterval;
-let packetTimeout;
 
 let file;
 let filename;
 let chunks;
 let flushing = false;
-let fileFinished = false;
-let timeoutLocked = false;
 
 let startTime, endTime, totalTime;
 
@@ -63,41 +58,38 @@ let startTime, endTime, totalTime;
     fileStream.pipe(file);
     fileStream.pipe(speaker);
     client.bind(port);    
-
 })();
 
 
 client.on('message', (message, remote)=>{
-    let packet = JSON.parse(message)
-    let header = packet.header; 
-    let body = packet.body;
+    let header = message.slice(0,1).readUInt8(0);
     
     switch(true)
     {
         case header == PACKET_INFO_INDEX:
-            // Saves the file details.
+            // Save any important information then initiate transfer.
             clearInterval(requestInterval);
-            // totalPackets = body.totalPackets;
-            // newPartition(body.partitionSize);
-            defaultPartitionSize = body.partitionSize;
-            chunks = new Array(body.partitionSize).fill(NO_DATA);
+
+            let partitionSize = message.slice(1,5).readUInt16BE(0);
+            defaultPartitionSize = partitionSize;
+            chunks = new Array(partitionSize).fill(NO_DATA);
             
             // Requests for the first partition.
             console.log('File details received. File transfer initiated.');
-            console.log('Partition Size is ' + body.partitionSize);
-                
-            sendPacket(INITIATE_TRANSFER, 0, remote, true);
-            // packetTimeout = getTimeout();
+            console.log('Partition Size is ' + partitionSize);
+
+            let nextPartition = Buffer.allocUnsafe(4);
+            nextPartition.writeUInt16BE(0);
+            sendPacket(INITIATE_TRANSFER, nextPartition, remote, true);
+
             totalTime = new Date();
             startTime = new Date();
             break;
 
         case header == PARTITION_PACKET:
-            // Starts storing the data into a buffer once the file details are received.
-            // clearInterval(requestInterval);
-            
-            let index = body.index;
-            let data = Buffer.from(body.data);
+            // Starts storing the data into a buffer once the file details are received.            
+            let index = message.slice(1, 5).readUInt16BE(0);
+            let data = message.slice(5);
             
             // The index of the packet in the partition
             let offset = defaultPartitionSize * partitionOffset;
@@ -105,34 +97,45 @@ client.on('message', (message, remote)=>{
 
             // Stores the data if it does not already exist.
             if(chunks[partitionIndex] == NO_DATA)
-            {
-                // clearInterval(packetTimeout);
-                // console.log('Received packet ' + (partitionIndex ) + ' from Partition ' + partitionOffset)
                 chunks[partitionIndex] = data;
-                // packetTimeout = getTimeout();
-            }
+            
             break;
 
         case header == PARTITION_FINISHED:
             // Processes the current partition and creates a new partition of the received Size.
             clearInterval(requestInterval)
-            // clearInterval(packetTimeout);
-            flushPartition(serverAddress, serverPort, body, ()=>{
-                console.log('Requesting for Partition '  + partitionOffset); 
-                sendPacket (INITIATE_TRANSFER, partitionOffset, remote, true);
-                // flushing = false;
-                // packetTimeout = getTimeout();
-            });
+
+            let missing = chunks.indexOf(NO_DATA);
+            if(missing > -1)
+            {
+                // Sends a negative acknowledgement for any missing packets.
+                let missingBuffer = Buffer.allocUnsafe(4);
+                let partitionBuffer = Buffer.allocUnsafe(4);
+
+                missingBuffer.writeUInt16BE(missing);
+                partitionBuffer.writeUInt16BE(partitionOffset);
+                let length = missingBuffer.length + partitionBuffer.length;
+
+                let data = Buffer.concat([missingBuffer, partitionBuffer], length);
+                sendPacket(MISSING_PACKET, data, remote, false);
+            }else
+            {
+                // Flushes the partition if it is complete.
+                let nextSize = message.slice(1,5).readUInt16BE(0);
+
+                flushPartition(nextSize, ()=>{
+                    console.log('Requesting for Partition '  + partitionOffset); 
+
+                    let partitionBuffer = Buffer.allocUnsafe(4);
+                    partitionBuffer.writeUInt16BE(partitionOffset);
+                    sendPacket (INITIATE_TRANSFER, partitionBuffer, remote, false);
+                });
+            }
             break;
 
         case header == FILE_TRANSFERRED:
             // Once the file is fully received, the client is triggered to close.
-            
-            console.log('Transferred file in ' +  (endTime-totalTime)/1000 + ' seconds');
-            clearInterval(requestInterval)
-            clearInterval(packetTimeout);
-            fileFinished = true;
-            console.log('File fully received.');
+            sendPacket (FILE_TRANSFERRED, null, remote, false);
             close();
             break;
 
@@ -141,113 +144,35 @@ client.on('message', (message, remote)=>{
     }
 });
 
-
-function getTimeout(){
-    
-    let interval = setInterval(() => {
-        console.log('Setting Timeout');
-        if(!timeoutLocked && !fileFinished)
-        {
-            console.log('Locking Timeout');
-            timeoutLocked = true;
-
-            if(partitionOffset == oldPartitionOffset)
-            {
-                // Checks if the 
-                console.log('Missed timeout at ' + partitionOffset);
-                flushPartition(serverAddress, serverPort, defaultPartitionSize, ()=>{
-                    console.log('Requesting for Partition '  + partitionOffset); 
-                    let remote = {
-                        'address': serverAddress,
-                        'port': serverPort
-                    };
-                    sendPacket (INITIATE_TRANSFER, partitionOffset, remote, true);
-                    
-                    console.log('Recovered from timeout');
-                });
-            }else
-            {
-                oldPartitionOffset = partitionOffset;
-            }
-            console.log('Unlocking Timeout');
-            timeoutLocked = false;
-            
-        }
-        else{
-            clearInterval(interval)
-            console.log('Timeout Locked');
-        } 
-    },100);
-    return interval;
-}
-
 /**
  * Fixes any errors in the file and flushes it out to the output buffer.
  * @param {String} serverAddress
  * @param {Integer} serverPort
  * @param {Integer} nextPartitionSize
  */
-function flushPartition(serverAddress, serverPort, nextPartitionSize, callback)
+function flushPartition(nextPartitionSize, callback)
 {
     if(flushing == false){
+        // Waits for the missing packets to be retreived before flushing the buffered packets.
         flushing = true;
-        requestMissingPackets(serverAddress, serverPort)
-        .then(()=>{
-            // Waits for the missing packets to be retreived before flushing the buffered packets.
+        
+        endTime = new Date();
+        let processTime = (endTime - startTime)/1000;
+        startTime = new Date()
 
-            endTime = new Date();
-            let processTime = (endTime - startTime)/1000;
-            // totalTime += processTime;
-            startTime = new Date()
-            console.log('Processed partition ' + partitionOffset + ' in ' + processTime + ' seconds');
-            
-            partitionOffset++;
-            // Writes the processed partition to the the readable stream buffer
-            chunks.forEach((element) =>{
-                fileStream.push(element)
-            });
+        console.log('Processed partition ' + partitionOffset + ' in ' + processTime + ' seconds');
+        partitionOffset++;
 
-            // Creates a new partition of a specified size.
-            chunks = new Array(nextPartitionSize).fill(NO_DATA);
-            flushing = false;
-            callback();
-        }).catch(()=>{
-            console.log('Promise unresolved');
+        // Writes the processed partition to the the readable stream buffer
+        chunks.forEach((element) =>{
+            fileStream.push(element)
         });
+
+        // Creates a new partition of a specified size.
+        chunks = new Array(nextPartitionSize).fill(NO_DATA);
+        flushing = false;
+        callback();
     }
-}
-
-/**
- * Requests for any missing packets until the specified index. 
- * @param {String} serverAddress
- * @param {Integer} serverPort
- * @param {function} callback
- */
-function requestMissingPackets(serverAddress, serverPort) 
-{
-    return new Promise((resolve, reject)=>{
-        // Looks for the indeces that have no data in them (which is set to 0).
-        let missing = chunks.indexOf(NO_DATA);
-        let interval = setInterval(()=>{
-            // Prevents the function from overflowing the buffer with requests.  
-            if(missing == -1)
-            {
-                clearInterval(interval)
-                // console.log('No missing');
-                return resolve();
-            }
-            
-            // Requests the missing packets of the current offset from the server.
-            let data = {
-                "missing": missing,
-                "partition": partitionOffset
-            }
-            let packet = makePacket(MISSING_PACKET, data);
-
-            client.send(JSON.stringify(packet), serverPort, serverAddress);
-            missing = chunks.indexOf(NO_DATA);
-        }, 0)        
-    });
 }
 
 client.on('listening', ()=>{
@@ -260,7 +185,6 @@ client.on('listening', ()=>{
 
     sendPacket(START_TRANSFER, null, remote, true);
 })  
-
 
 /**
  * Error handling.
@@ -280,17 +204,15 @@ client.on('error', (err) => {
 function sendPacket(header, body, remote, essential)
 {
     let packet = makePacket(header,body);
-    client.send(JSON.stringify(packet), remote.port, remote.address)
+    client.send(packet, remote.port, remote.address)
     if(essential)
     {
         // Checks if essential packets have been acknowledged by the server.
         requestInterval = setInterval(()=>{
-            // Attempts to reconnect to the server after 1500ms
-            // console.log('Next partition not received after 1500ms.');
             console.log('Timeout on Partition ' + body);
             console.log('Attempting to reconnect...');
-            client.send(JSON.stringify(packet), remote.port, remote.address);
-        }, 100);
+            client.send(packet, remote.port, remote.address);
+        }, REQUEST_INTERVAL);
     }
 }
 
@@ -300,20 +222,31 @@ function sendPacket(header, body, remote, essential)
  */
 function close()
 {
+    clearInterval(requestInterval)
+    console.log('Transferred file in ' + (endTime-totalTime)/1000 + ' seconds');
+    console.log('File fully received.');
+
+    // Ends the read stream.
     fileStream.push(null);
-    console.log('Waiting for audio to finish.')
-    speaker.on('finish', () => {
-        file.end();
-        client.close();
+    console.log('Waiting for audio to finish.');
+    speaker.on('finish', ()=>{
         console.log('File Outputted as ' + filename);
         console.log('Closing client.');
-    });
+        file.end();
+        client.close();
+    })
 }
 
+/**
+ * Creates a packet from a buffer with its type indicated by its first byte (header).
+ * Any following information specific to the type of packet should be appended after it.
+ * @param {Integer} header One of the message protocols
+ * @param {Buffer} body A buffer containing the data that will be sent.
+ */
 function makePacket(header, body)
 {
-    return {
-        'header': header,
-        'body' : body
-    }
+    let headerBuffer = Buffer.allocUnsafe(1);
+    headerBuffer.writeUInt8(header);
+    if(body == null) return headerBuffer;
+    return Buffer.concat([headerBuffer, body], (headerBuffer.length + body.length));
 }
--- Server/Server.js
+++ Server/Server.js
@@ -4,13 +4,13 @@ const dgram = require('dgram');
 const socket = dgram.createSocket('udp4');
 
 // Protocol messages
-const START_TRANSFER = 'Start Transfer';
-const PACKET_INFO_INDEX = 'Packet Info';
-const PARTITION_PACKET = 'Partition Packet';
-const INITIATE_TRANSFER = 'Initiate Partition Transfer';
-const PARTITION_FINISHED = 'Partition Sent';
-const FILE_TRANSFERRED = 'Finished';
-const MISSING_PACKET = 'Missing Packet'
+const START_TRANSFER = 0;
+const PACKET_INFO_INDEX = 1;
+const PARTITION_PACKET = 2;
+const INITIATE_TRANSFER = 3;
+const PARTITION_FINISHED = 4;
+const FILE_TRANSFERRED = 5;
+const MISSING_PACKET = 6;
 
 // Server port
 let port;
@@ -22,6 +22,10 @@ let file;
 let filebuffer = [];
 let packetsPerPartition;
 let numChunks;
+
+let resendUnacked;
+let missedPartition = 0;
+let missedPacket = 0;
 
 (function (){
     let args = process.argv;
@@ -39,7 +43,6 @@ let numChunks;
     socket.bind(port);   
 })();
 
-
 /**
  * Loads the file once the server is running.
  */
@@ -47,7 +50,7 @@ socket.on('listening', () => {
     let address = socket.address();
     console.log('Buffering audio file...');
     bufferFile();
-    console.log(filebuffer.length);
+    console.log(packetsPerPartition);
     console.log(filename + " hosted on " + address.address + ':' + address.port);
 });
 
@@ -60,45 +63,56 @@ socket.on('error', (err) => {
 })
 
 socket.on('message', (message, remote)=>{
-    let packet = JSON.parse(message);
-    let header = packet.header;
-    let data = packet.body;
-    switch(header)
+    let packet;
+    let header = message.slice(0,1).readUInt8(0);
+    switch(true)
     {
-        case START_TRANSFER:
+        case header == START_TRANSFER:
             // Sends the file details 
             console.log('Streaming to ' + remote.address + ':' + remote.port);
             console.log('Sending Partitions...');
-            let fileDetails = {
-                "totalPackets" : numChunks,
-                "partitionSize" : packetsPerPartition,
-                'numPartitions' : filebuffer.length
-            }
-            packet = makePacket(PACKET_INFO_INDEX,fileDetails);
+
+            let numberOfPackets = Buffer.allocUnsafe(4);
+            numberOfPackets.writeUInt16BE(packetsPerPartition);
+            packet = makePacket(PACKET_INFO_INDEX,numberOfPackets);
 
             console.log('Sending File Details')
-            socket.send(JSON.stringify(packet), remote.port, remote.address)
+            socket.send(packet, remote.port, remote.address)
             break;
-        case INITIATE_TRANSFER:
+        case header == INITIATE_TRANSFER:
             // Sends a specified partition of the file.
+            clearInterval(resendUnacked);
+            let data = message.slice(1).readUInt16BE(0);
             if (data < filebuffer.length) 
+            {
+                // Sends the unacked packets when the timeout expires.
+                resendUnacked = setInterval(()=>{
+                    console.log('Timed out at ' + missedPartition);
+                    sendPacket(missedPacket, missedPartition, false, remote);
+                }, 100)
                 console.log('Initiate Transfer of partition ' + data)
-            sendPacket(0, data, false, remote);
+                sendPacket(0, data, false, remote);
+            }
+            
             break;
-        case MISSING_PACKET:
+        case header == MISSING_PACKET:
             // Sends the missed packets.
-            let missedPacket = data.missing;
-            let partition = data.partition;
-            // console.log('Missed Packet ' + missedPacket + ' of Partition ' + partition);
+            missedPacket = message.slice(1,5).readUInt16BE(0);
+            missedPartition = message.slice(5,9).readUInt16BE(0);
+
             // Sends the packet information again if it hasn't been received.
-            sendPacket(missedPacket, partition, true , remote);
+            sendPacket(missedPacket, missedPartition, false , remote);
             break;
+        case header == FILE_TRANSFERRED:
+            console.log('File has been fully transmitted.');
+            clearInterval(resendUnacked);
+            break;
+
         default:
-            console.log('Error unknown command.')
+            console.log('Unknown command.');
     }
 })
 
-// TODO: Can probably change this to include the header as an argument.
 /**
  * Sends a number of packets starting from the stored index.
  * @param {Integer} index 
@@ -111,56 +125,30 @@ function sendPacket(index, partitionIndex, sendOne, remote)
     let partition = filebuffer[partitionIndex]
     let packet;
 
-    // Sends packets starting from an index
-    if(partitionIndex >= filebuffer.length)
-    {
-        packet = makePacket(FILE_TRANSFERRED, null)
-        socket.send(JSON.stringify(packet), remote.port, remote.address);
-        console.log('File has been fully transmitted.');
+    if(index >= partition.length)
+    { 
+        // Sends the information of the next partition once the end of the current partition is reached.
+        let header = (partitionIndex == filebuffer.length - 1 )? FILE_TRANSFERRED : PARTITION_FINISHED;
+        let nextSize = (partitionIndex == filebuffer.length - 1)? 0 : filebuffer[partitionIndex + 1].length;
+        let nextSizeBuffer = Buffer.allocUnsafe(4);
+        nextSizeBuffer.writeUInt16BE(nextSize);
+        
+        packet = makePacket(header, nextSizeBuffer);
+        socket.send(packet, remote.port, remote.address,(err)=>{
+            if(err)throw err;
+            console.log('Partition ' + partitionIndex + ' empty.');
+        });
     }else
     {
-        let interval = setInterval(()=>{
-            if(index >= partition.length)
-            { 
-                // Sends the information of the next partition once the end of the current
-                // partition is reached.
-                clearInterval(interval);
-                console.log('Partition ' + partitionIndex + ' empty.');
-                let nextSize = (partitionIndex == filebuffer.length - 1)? 0 : filebuffer[partitionIndex + 1].length;
-                let header = (partitionIndex == filebuffer.length - 1 )? FILE_TRANSFERRED : PARTITION_FINISHED;
-
-                // send  Partition index as body for concurrent thing
-                packet = makePacket(header, nextSize);
-            }else
-            {
-                if(sendOne) clearInterval(interval);
-                packet = makePacket(PARTITION_PACKET, partition[index++]);            
-            }
-            
-            socket.send(JSON.stringify(packet), remote.port, remote.address);
-        }, 0);
+        // Sends packets in the partition.
+        packet = makePacket(PARTITION_PACKET, partition[index++]);    
+    
+        socket.send(packet, remote.port, remote.address,(err)=>{
+            if(err)throw err;        
+            if(!sendOne ) sendPacket(index, partitionIndex,sendOne, remote);            
+        });
     }
 
-    // switch(true)
-    // {
-    //     case partitionIndex >= filebuffer.length:
-    //         // Notifies the client that the file has been fully transferred
-    //         packet = makePacket(FILE_TRANSFERRED, null)
-    //         socket.send(JSON.stringify(packet), remote.port, remote.address);
-    //         console.log('File has been fully transmitted.');
-    //         return;
-    //     case sendOne:
-    //         packet = makePacket(PARTITION_PACKET, partition[index++]);
-    //         socket.send(JSON.stringify(packet), remote.port, remote.address);
-    //         break;
-    //     case index < partition.length:
-            
-            
-    //         break;
-    //     default:
-    //         // Notifies the client that the partition is empty by default.
-    //         console.log('Reaches Default');
-    // }
 }
 
 /**
@@ -170,9 +158,9 @@ function sendPacket(index, partitionIndex, sendOne, remote)
 function bufferFile()
 {
     let filesize = file.length;
-    let chunkSize = 1024 * 15;
+    let chunkSize = 1024;
     numChunks = Math.ceil(filesize/chunkSize, chunkSize);
-    let index = 0; // Chunk index
+    let chunkIndex = 0; 
 
     // Divides the number of packets into percentages.
     const numPartitions = 100;
@@ -180,19 +168,23 @@ function bufferFile()
     let partition = new Array();
     
     
-    while(index < numChunks)
+    while(chunkIndex < numChunks)
     {
         // Stores the packet into a partition.
-        let offset = index * chunkSize;
-        let buffer = file.slice(offset, chunkSize + offset);
-        let packet = {
-            'index' : index,
-            'data' : buffer
-        }
-        partition.push(packet);
-        index++;
+        let offset = chunkIndex * chunkSize;
+        let fileSlice = file.slice(offset, chunkSize + offset);
+
+        // Writes the index to a 4 byte sequence number.
+        let indexBuffer = Buffer.allocUnsafe(4);
+        indexBuffer.writeUInt16BE(chunkIndex);
+        let totalLength = indexBuffer.length + fileSlice.length;
+
+        // Stores the new packet into the partition
+        let buffer = Buffer.concat([indexBuffer, fileSlice], totalLength);
+        partition.push(buffer);
+        chunkIndex++;
         
-        if(((index % packetsPerPartition) == 0 && index != 0) || index == numChunks-1)
+        if(((chunkIndex % packetsPerPartition) == 0 && chunkIndex != 0) || chunkIndex == numChunks-1)
         {
             // Adds the partition into the filebuffer when it is full or if it is the 
             // last partition of the file.
@@ -203,14 +195,15 @@ function bufferFile()
 }
 
 /**
- * Creates and returns packet object.
- * @param {Object} header 
- * @param {Object} body 
+ * Creates a packet from a buffer with its type indicated by its first byte (header).
+ * Any following information specific to the type of packet should be appended after it.
+ * @param {Integer} header One of the message protocols
+ * @param {Buffer} body A buffer containing the data that will be sent.
  */
 function makePacket(header, body)
 {
-    return {
-        'header': header,
-        'body': body
-    }
+    let headerBuffer = Buffer.allocUnsafe(1);
+    headerBuffer.writeUInt8(header);
+    if(body == null) return headerBuffer;
+    return Buffer.concat([headerBuffer, body], (headerBuffer.length + body.length))
 }
